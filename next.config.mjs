f193d7266f507f9d90c989f2cbe2a77fbf564364
ShakeyPlays <skenyon6@uwo.ca>
2026-03-20 00:50:14 -0400

    Update Next.js configuration for export and images

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
       },
     ],
+  },
+  basePath: '/SAOKwebsite',
+  assetPrefix: '/SAOKwebsite',
+
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
